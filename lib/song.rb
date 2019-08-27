3d7dc41ed98b41ba857a5659bec35016d7742def
Chantel Lowe <chantellowe17@gmail.com>
2019-08-27 07:34:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
     self.find_by_name(name) || self.create_by_name(name)
   end
   
-  def self.alphabetical(name)
+  def self.alphabetical
     @@all.sort{ |a,b| a.name <=> b.name }
   end  
   
